2cb6518d6d2a115b42b81e666645c7ba6a69e45c
Phi Hung <phihung.devka@gmail.com>
2025-07-09 17:06:25 +0700

    haizz 2

--- src/features/share-directory/user/pages/home/Home.tsx
+++ src/features/share-directory/user/pages/home/Home.tsx
@@ -1,16 +1,14 @@
 // import styles from "./Home.module.scss";
-import { Carousel, Spin } from "antd";
+import { Carousel } from "antd";
 import { ProductList } from "@repo/component/ui/list/ProductList";
 import { bannerApi, homeApi } from "@repo/packages/services";
-import { LoadingOutlined } from "@ant-design/icons";
 
 const Home = () => {
     const bannerQuery = bannerApi.queries.readQuery();
-    const { data: homeProduct, isLoading: homeLoading } = homeApi.queries.readQuery();
+    const homeProduct = homeApi.queries.readQuery();
 
     return (
         <section>
-            <Spin spinning={homeLoading} indicator={<LoadingOutlined spin />} size="large" fullscreen />
             <div className="container-fluid">
                 <Carousel draggable autoplay={{ dotDuration: true }} autoplaySpeed={4000} pauseOnHover={false}>
                     {bannerQuery.data?.map((item) => (
@@ -20,7 +18,7 @@ const Home = () => {
             </div>
             <div className="container">
                 <div>
-                    <ProductList products={homeProduct ?? []} />
+                    <ProductList products={homeProduct.data ?? []} />
                 </div>
             </div>
         </section>
